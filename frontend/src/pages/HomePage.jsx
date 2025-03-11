8222c3fcd42833efb7ff5dd885ae664e69272784
Satya <satyajitborade99@gmail.com>
2025-03-11 22:10:23 +0530

    login corrected

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
+function AdminDashboard() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <h1>Welcome to HomePage</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+}
+
+export default AdminDashboard;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const Login = () => {
+function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
